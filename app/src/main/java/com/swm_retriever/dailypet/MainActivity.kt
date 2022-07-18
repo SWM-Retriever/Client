4c4abf8e3a530e05a5686794f637ae8ee5db0609
ashpurple <ashpurple9@gmail.com>
2022-07-18 17:02:56 +0900

    Init: Create Project

---
+++ app/src/main/java/com/swm_retriever/dailypet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swm_retriever.dailypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
