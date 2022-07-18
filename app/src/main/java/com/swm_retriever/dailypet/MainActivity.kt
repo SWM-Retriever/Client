c9e99058ca74e93b67c50ea782abac4f9f39bcdf
ashpurple <ashpurple9@gmail.com>
2022-07-18 23:56:27 +0900

    Init: Firebase Crashlytics Test

--- app/src/main/java/com/swm_retriever/dailypet/MainActivity.kt
+++ app/src/main/java/com/swm_retriever/dailypet/MainActivity.kt
@@ -2,10 +2,24 @@ package com.swm_retriever.dailypet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = findViewById<Button>(R.id.btn_crash)
+        crashButton.setOnClickListener{
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+//        addContentView(crashButton, ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
+
+
+
 }
