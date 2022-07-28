52406edb5670b1fd4b345fdc040fd78dc76de602
ashpurple <ashpurple9@gmail.com>
2022-07-28 15:38:42 +0900

    Create: Splash Fade-In Animation

--- app/src/main/java/org/retriever/dailypet/SplashActivity.kt
+++ app/src/main/java/org/retriever/dailypet/SplashActivity.kt
@@ -1,21 +1,29 @@
 package org.retriever.dailypet
-
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        var imageLogo = findViewById<ImageView>(R.id.img_logo)
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.anim_fadein)
+        imageLogo.startAnimation(fadeIn)
+
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        },DURATION)
-
+        }, DURATION)
     }
     companion object {
         private const val DURATION : Long = 1000
