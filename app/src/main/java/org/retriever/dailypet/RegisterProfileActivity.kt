9e53b6ec5299994e0c3e50ba67abebfb29f3c7e2
ashpurple <ashpurple9@gmail.com>
2022-07-29 18:12:28 +0900

    PET-101 Create: RegisterProfile Wireframe

---
+++ app/src/main/java/org/retriever/dailypet/RegisterProfileActivity.kt
@@ -0,0 +1,11 @@
+package org.retriever.dailypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_profile)
+    }
+}
