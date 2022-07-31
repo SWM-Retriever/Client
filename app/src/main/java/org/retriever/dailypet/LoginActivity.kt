62e02ae4f4c885a58f7ec6347e4cf4ba7ad74518
ashpurple <ashpurple9@gmail.com>
2022-08-01 00:20:42 +0900

    PET-168 Add: Login success process

--- app/src/main/java/org/retriever/dailypet/LoginActivity.kt
+++ app/src/main/java/org/retriever/dailypet/LoginActivity.kt
@@ -4,15 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageButton
 import androidx.lifecycle.lifecycleScope
-import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.launch
+import org.retriever.dailypet.databinding.ActivityLoginBinding
 
 class GlobalApplication : Application() {
     override fun onCreate() {
@@ -22,21 +20,25 @@ class GlobalApplication : Application() {
 }
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private var TAG = "LOGIN"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val keyHash = Utility.getKeyHash(this)
-        Log.e("Key ",keyHash)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
         val context = this
-        val kakaoLoginButton = findViewById<ImageButton>(R.id.btn_kakaoLogin)
-        var naverLoginButton = findViewById<ImageButton>(R.id.btn_naverLogin)
 
-        kakaoLoginButton.setOnClickListener {
-            lateinit var token : OAuthToken
+        binding.btnKakaoLogin.setOnClickListener {
+            var kakaoLogin : Boolean = false
             lifecycleScope.launch {
                 try {
-                    token = UserApiClient.loginWithKakao(context)
-                    Log.e("LoginActivity", "Token:  > $token")
+                    var token = UserApiClient.loginWithKakao(context)
+                    Log.d(TAG, "Token:  > $token")
+                    Intent(this@LoginActivity, RegisterProfileActivity::class.java).also{
+                        startActivity(it)
+                        finish()
+                    }
                 } catch (error: Throwable) {
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
                         Log.d("LoginActivity", "사용자가 명시적으로 취소")
@@ -45,12 +47,8 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             }
-            if(token != null) {
-                val nextIntent = Intent(this, RegisterProfileActivity::class.java)
-                startActivity(nextIntent)
-            }
         }
-        naverLoginButton.setOnClickListener{
+        binding.btnNaverLogin.setOnClickListener{
             val nextIntent = Intent(this, RegisterProfileActivity::class.java)
             startActivity(nextIntent)
         }
