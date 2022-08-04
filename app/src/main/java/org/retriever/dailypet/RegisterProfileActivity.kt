b9a96af8c9af0157f75fe56d453b72ce505846a3
ashpurple <ashpurple9@gmail.com>
2022-08-04 14:44:10 +0900

    Refactoring: noActionbar, empty nickname handling

--- app/src/main/java/org/retriever/dailypet/RegisterProfileActivity.kt
+++ app/src/main/java/org/retriever/dailypet/RegisterProfileActivity.kt
@@ -117,7 +117,11 @@ class RegisterProfileActivity : AppCompatActivity() {
         binding.btnRegisterProfileCheck.setOnClickListener{
             Log.d(TAG, "Button NickName Check")
             val nickname = binding.textRegisterProfileNickName.text.toString()
-            checkValidNickname(nickname)
+            if(nickname == ""){
+                binding.textRegisterProfileValidate.text = "올바른 닉네임을 입력해주세요"
+                binding.textRegisterProfileValidate.setTextColor(Color.BLACK)
+            }
+            else checkValidNickname(nickname)
         }
         /* Submit Profile */
         binding.btnRegisterProfileSubmit.setOnClickListener{
