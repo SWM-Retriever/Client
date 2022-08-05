34adc664542e31ed5790bee95e099ea0009a6633
ashpurple <ashpurple9@gmail.com>
2022-08-05 13:28:04 +0900

    PET-114 Create: findFamily UI

---
+++ app/src/main/java/org/retriever/dailypet/FindFamilyActivity.kt
@@ -0,0 +1,11 @@
+package org.retriever.dailypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindFamilyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_family)
+    }
+}
