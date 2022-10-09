b5c0f9111d5ac7534fb9b050e1327bbbf73fb8d4
ashpurple <ashpurple9@gmail.com>
2022-10-09 22:06:42 +0900

    Refactor: fragment_find_group.xml

--- app/src/main/java/org/retriever/dailypet/ui/signup/FindGroupFragment.kt
+++ app/src/main/java/org/retriever/dailypet/ui/signup/FindGroupFragment.kt
@@ -1,15 +1,19 @@
 package org.retriever.dailypet.ui.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
 import org.retriever.dailypet.R
 import org.retriever.dailypet.databinding.FragmentFindGroupBinding
 import org.retriever.dailypet.ui.base.BaseFragment
+import org.retriever.dailypet.ui.main.MainActivity
 import org.retriever.dailypet.util.hideProgressCircular
+import org.retriever.dailypet.util.setViewBackgroundWithoutResettingPadding
 
 class FindGroupFragment : BaseFragment<FragmentFindGroupBinding>() {
 
@@ -33,40 +37,49 @@ class FindGroupFragment : BaseFragment<FragmentFindGroupBinding>() {
 
     private fun buttonClick() = with(binding) {
 
-        btnSearchInviteCode.setOnClickListener {
-            val inviteCode = editTextCode.text.toString()
+        searchInviteButton.setOnClickListener {
+            val inviteCode = inputCodeEdittext.text.toString()
             if (inviteCode.isBlank()) {
-                textInviteCodeValidate.text = "올바른 코드를 입력해주세요"
-                textInviteCodeValidate.setTextColor(ContextCompat.getColor(requireContext(), R.color.fail_red))
-                editTextCode.background = ContextCompat.getDrawable(requireContext(), R.drawable.fail_edittext)
+                inviteCodeValidateText.text = "유효하지 않은 초대코드입니다"
+                inviteCodeValidateText.setTextColor(ContextCompat.getColor(requireContext(), R.color.fail_red))
+                inputCodeEdittext.setViewBackgroundWithoutResettingPadding(R.drawable.fail_edittext)
                 isValidCode = false
                 setVisibility()
             } else {
+                inviteCodeValidateText.text = "유효한 초대코드입니다"
+                inviteCodeValidateText.setTextColor(ContextCompat.getColor(requireContext(), R.color.success_blue))
+                inputCodeEdittext.setViewBackgroundWithoutResettingPadding(R.drawable.success_edittext)
+                isValidCode = true
+                setVisibility()
                 // TODO 가족 코드 검증 로직
             }
         }
 
         /* Register Family Member Page*/
-        btnAddProfile.setOnClickListener {
+        enterGroupButton.setOnClickListener {
             if (isValidCode) {
-//                val nextIntent = Intent(requireContext(), EnterFamilyActivity::class.java)
-//                nextIntent.putExtra("FamilyName", groupName)
-//                startActivity(nextIntent) // 가족 구성원 등록 페이지로 이동
+                val nextIntent = Intent(requireContext(), MainActivity::class.java)
+                nextIntent.putExtra("FamilyName", groupName)
+                startActivity(nextIntent) // 가족 구성원 등록 페이지로 이동
             }
+        }
+
+        backButton.setOnClickListener {
+            root.findNavController().popBackStack()
         }
     }
 
     private fun setVisibility() = with(binding) {
         if (isValidCode) {
             imgFamilyPhoto.visibility = View.VISIBLE
-            textFamilyName.visibility = View.VISIBLE
-            btnAddProfile.visibility = Button.VISIBLE
-            textInvisibleComment.visibility = View.VISIBLE
+            groupNameText.visibility = View.VISIBLE
+            enterGroupButton.visibility = Button.VISIBLE
+            findGroupInvisibleCommentText.visibility = View.VISIBLE
         } else {
             imgFamilyPhoto.visibility = View.INVISIBLE
-            textFamilyName.visibility = View.INVISIBLE
-            btnAddProfile.visibility = Button.INVISIBLE
-            textInvisibleComment.visibility = View.INVISIBLE
+            groupNameText.visibility = View.INVISIBLE
+            enterGroupButton.visibility = Button.INVISIBLE
+            findGroupInvisibleCommentText.visibility = View.INVISIBLE
         }
     }
 
