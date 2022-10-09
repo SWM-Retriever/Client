01893a477e1c41b9eb772e563e060e6b4295c20e
koreatlwls <koreatlwls@gmail.com>
2022-10-10 02:54:07 +0900

    feat : Implementation diary today

--- app/src/main/java/org/retriever/dailypet/model/diary/DiaryItem.kt
+++ app/src/main/java/org/retriever/dailypet/model/diary/DiaryItem.kt
@@ -7,9 +7,9 @@ import java.util.*
 data class DiaryItem(
     val viewType: String,
     val date: String,
-    val diaryId: Int,
-    val authorImageUrl: String,
-    val authorNickName: String,
-    val diaryImageUrlList: List<String>,
-    val diaryText: String,
+    val diaryId: Int?,
+    val authorImageUrl: String?,
+    val authorNickName: String?,
+    val diaryImageUrlList: List<String>?,
+    val diaryText: String?,
 )
--- app/src/main/java/org/retriever/dailypet/ui/main/diary/adapter/DateViewHolder.kt
+++ app/src/main/java/org/retriever/dailypet/ui/main/diary/adapter/DateViewHolder.kt
@@ -3,11 +3,12 @@ package org.retriever.dailypet.ui.main.diary.adapter
 import androidx.recyclerview.widget.RecyclerView
 import org.retriever.dailypet.databinding.ItemDateBinding
 import org.retriever.dailypet.model.diary.DiaryItem
+import org.retriever.dailypet.util.getCurrentDate
 
 class DateViewHolder(private val binding : ItemDateBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item : DiaryItem){
-        binding.dateText.text = item.date
+        binding.dateText.text = if(item.date == getCurrentDate()) "Today" else item.date
     }
 
 }
