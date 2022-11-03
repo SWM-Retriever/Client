e1421f31d343db6929ab16603029c58e8f0798ec
koreatlwls <koreatlwls@gmail.com>
2022-11-03 23:53:10 +0900

    refactoring : TermsOfService && CreateProfile

--- app/src/main/java/org/retriever/dailypet/ui/signup/firstcourse/ProfileViewModel.kt
+++ app/src/main/java/org/retriever/dailypet/ui/signup/firstcourse/ProfileViewModel.kt
@@ -1,10 +1,12 @@
-package org.retriever.dailypet.ui.signup.viewmodel
+package org.retriever.dailypet.ui.signup.firstcourse
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -23,6 +25,70 @@ class ProfileViewModel @Inject constructor(
     private val profileRepository: ProfileRepository,
     private val preSignedUrlRepository: PreSignedUrlRepository,
 ) : ViewModel() {
+
+    private val _allCheckState = MutableStateFlow(false)
+    val allCheckState: StateFlow<Boolean> = _allCheckState
+
+    private val _firstCheckState = MutableStateFlow(false)
+    val firstCheckState: StateFlow<Boolean> = _firstCheckState
+
+    private val _secondCheckState = MutableStateFlow(false)
+    val secondCheckState: StateFlow<Boolean> = _secondCheckState
+
+    private val _thirdCheckState = MutableStateFlow(false)
+    val thirdCheckState: StateFlow<Boolean> = _thirdCheckState
+
+    private val _nextButtonState = MutableStateFlow(false)
+    val nextButtonState: StateFlow<Boolean> = _nextButtonState
+
+    private val _nickNameViewState = MutableLiveData(NickNameViewState.DEFAULT_STATE)
+    val nickNameViewState: LiveData<NickNameViewState> = _nickNameViewState
+
+    private val _registerButtonState = MutableStateFlow(false)
+    val registerButtonState : StateFlow<Boolean> = _registerButtonState
+
+    // TODO CreateProfileFragment 이미지 상태 저장
+
+    fun setAllCheckState(check: Boolean) {
+        _allCheckState.value = check
+        _firstCheckState.value = check
+        _secondCheckState.value = check
+        _thirdCheckState.value = check
+        setNextButtonState()
+    }
+
+    fun setFirstCheckState(check: Boolean) {
+        _firstCheckState.value = check
+        setAllCheckState()
+        setNextButtonState()
+    }
+
+    fun setSecondCheckState(check: Boolean) {
+        _secondCheckState.value = check
+        setAllCheckState()
+        setNextButtonState()
+    }
+
+    fun setThirdCheckState(check: Boolean) {
+        _thirdCheckState.value = check
+        setAllCheckState()
+    }
+
+    private fun setAllCheckState() {
+        _allCheckState.value = _firstCheckState.value && _secondCheckState.value && _thirdCheckState.value
+    }
+
+    private fun setNextButtonState() {
+        _nextButtonState.value = _firstCheckState.value && _secondCheckState.value
+    }
+
+    fun setNickNameState(state : NickNameViewState){
+        _nickNameViewState.value = state
+    }
+
+    fun setRegisterButtonState(check : Boolean){
+        _registerButtonState.value = check
+    }
 
     private val _nickNameResponse = MutableLiveData<Resource<ResponseBody>>()
     val nickNameResponse: LiveData<Resource<ResponseBody>> = _nickNameResponse
