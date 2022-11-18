f44d20c9acbfe8fd976bde4c4f41c977ee7b4a94
ashpurple <ashpurple9@gmail.com>
2022-11-18 17:08:01 +0900

    Temp: UpdateManger Test

--- app/src/main/java/org/retriever/dailypet/ui/splash/SplashActivity.kt
+++ app/src/main/java/org/retriever/dailypet/ui/splash/SplashActivity.kt
@@ -1,10 +1,13 @@
 package org.retriever.dailypet.ui.splash
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.Toast
@@ -13,8 +16,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.findNavController
+import com.google.android.gms.tasks.Task
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
 import com.google.android.play.core.install.model.UpdateAvailability
 import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.common.model.KakaoSdkError
@@ -41,6 +50,7 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>({ ActivitySplashBindi
     private val loginViewModel by viewModels<LoginViewModel>()
     private val REQUEST_CODE_UPDATE = 1
     private val jwt = GlobalApplication.prefs.jwt ?: ""
+    private lateinit var appUpdateManager : AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,57 +67,76 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>({ ActivitySplashBindi
 
     override fun onResume() {
         super.onResume()
-
-        val appUpdateManager = AppUpdateManagerFactory.create(this)
-
+        appUpdateManager = AppUpdateManagerFactory.create(this)
         appUpdateManager
             .appUpdateInfo
             .addOnSuccessListener { appUpdateInfo ->
                 if (appUpdateInfo.updateAvailability()
                     == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
                 ) {
-                    // If an in-app update is already running, resume the update.
-                    appUpdateManager.startUpdateFlowForResult(
-                        appUpdateInfo,
-                        AppUpdateType.IMMEDIATE,
-                        this,
-                        REQUEST_CODE_UPDATE
-                    )
+                    requestUpdate(appUpdateInfo)
                 }
             }
     }
 
-
-
     private fun checkUpdate(){
-        val appUpdateManager = AppUpdateManagerFactory.create(this)
-
+        appUpdateManager = AppUpdateManagerFactory.create(this)
         appUpdateManager.let {
             it.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+                Log.e("ABC Availability",appUpdateInfo.updateAvailability().toString())
+                Log.e("ABC Version",appUpdateInfo.availableVersionCode().toString())
                 if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-                    // or AppUpdateType.FLEXIBLE
-                    appUpdateManager.startUpdateFlowForResult(
-                        appUpdateInfo,
-                        AppUpdateType.IMMEDIATE, // or AppUpdateType.FLEXIBLE
-                        this,
-                        REQUEST_CODE_UPDATE
-                    )
+                    && appUpdateInfo.isUpdateTypeAllowed(IMMEDIATE)) {
+                    requestUpdate(appUpdateInfo)
                 }
             }
         }
+    }
+
+    private fun requestUpdate(appUpdateInfo: AppUpdateInfo){
+        Log.e("ABC","Update")
+        appUpdateManager.startUpdateFlowForResult(
+            appUpdateInfo,
+            IMMEDIATE,
+            this,
+            REQUEST_CODE_UPDATE
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_UPDATE) {
-            if (resultCode != RESULT_OK) {
-                Toast.makeText(this,"업데이트에 실패하였습니다",Toast.LENGTH_SHORT).show()
+            if (resultCode != Activity.RESULT_OK) {
+                appUpdateManager.let {
+                    it.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+                        if (appUpdateInfo.updateAvailability() === UpdateAvailability.UPDATE_AVAILABLE) {
+                            if (appUpdateInfo.isUpdateTypeAllowed(IMMEDIATE)) {
+                                val dialog = MaterialAlertDialogBuilder(this, R.style.CustomMaterialAlertDialog)
+                                    .setTitle("업데이트 알림",)
+                                    .setMessage("필수 업데이트가 있습니다.\n최적의 사용 환경을 위해 최신 버전으로 업데이트 해주세요.")
+                                    .setNegativeButton("취소") { dialog, _ ->
+                                        dialog.dismiss()
+                                        finish()
+                                    }
+                                    .setPositiveButton("업데이트") { _, _ ->
+                                        requestUpdate(appUpdateInfo)
+                                    }
+                                dialog.show()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-
+//    private fun popupSnackbarForCompleteUpdate() {
+//        val snackbar = Snackbar.make(findViewById(R.id.clActivityMain), "업데이트 버전 다운로드 완료", 5000)
+//            .setAction("설치/재시작") {
+//                appUpdateManager?.completeUpdate()
+//            }
+//
+//        snackbar.show()
+//    }
 
     private fun initProgress() = with(binding) {
         val loginIntent = Intent(applicationContext, LoginActivity::class.java)
